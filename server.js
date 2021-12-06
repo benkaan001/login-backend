025aec16bae5eda8f77b5761d6dd55d10cad7cd7
Ben Kaan <benkaan001@gmail.com>
2021-12-05 23:25:38 -0600

    set up ejs views and routes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("heeeya!");
+const express = require("express");
+
+const app = express();
+
+// set the viewengine to ejs
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("main.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`🌍------Server is listening on port ${port}------🌍`);
+});
